10657bd0dde3f17cb53a4436ee9128e1037eedd0
jiazexin <jiazexin@sensetime.com>
2019-06-26 15:04:47 +0800

    重构

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * weblog
+ * 前端日志上报解决方案
+ * Author: Zexing
+ * Date: 2019/07/01
+ */
+
+import MonitorForWeb from './MonitorForWeb'
+window.MonitorForWeb = MonitorForWeb ;
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+let path = require('path');
+module.exports = {
+    entry: './src/index.js',//入口文件
+    output: {//出口配置
+        path: path.resolve('dist'),//出口文件路径
+        filename: "[name].[chunkhash:8].js"//出口文件名称
+    },
+    devtool: "source-map",
+    module: {
+        rules:
+            [
+                {
+                    test: /\.worker\.js$/,
+                    use: {
+                        loader: 'worker-loader',
+                        options: { inline: true, fallback: false}
+                    },
+                },
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader'
+                    },
+                    exclude: /node_modules/
+                }
+            ]
+    }
+};
